54f42b9515d9a9a25f9c99e200071c4b7dd4b4bc
Wharkk <76532812+Wharkk@users.noreply.github.com>
2021-02-06 14:52:50 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,56 @@
+<footer class="page-footer font-small blue py-3 " style="background: #999999;width:100%;margin-top:300px;">
+  <div class="container-fluid text-center text-md-left">
+    <div class="row">
+      <div class="col-md-4 mt-md-0 mt-3">
+
+        <h5 class="text-uppercase">Nucléarium MCPE</h5>
+        <p>Serveur PvP Faction moddé</p>
+
+      </div>
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <div class="col-md-4 mb-md-0 mb-3" style="text-align: center">
+
+        <h5 class="text-uppercase">
+        	<a target="_blank" href="https://discord.com/invite/qDCtX6B" role="button" class="btn btn-light"><i class="fab fa-discord" aria-hidden="true"></i></a>
+        	<a target="_blank" href="https://www.youtube.com/c/dadodasyra/" role="button" class="btn btn-light"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+        	<a href="#" role="button" class="btn btn-light" data-toggle="modal" data-target="#mail"><i class="fas fa-envelope-open-text" aria-hidden="true"></i></a>
+        </h5>
+        <div class="footer-copyright text-center py-3" style="padding-top: 1px;">© 2020 Copyright:
+          <a href="../index.php" class="text-dark"> Nuclearium FR</a>
+          <p class="text-center" style="margin-bottom:0;"><small>Fondateur de Nuclearium : OrRubiss#....</small></p>
+          <p class="text-center"><small>Développeurs du site : Wharkk#0827, ablayeYT#.... et PPC#.... </small></p>
+        </div>
+
+      </div>
+		<hr class="clearfix w-100 d-md-none pb-3">
+		<div class="col-md-1">
+
+      </div>
+      <div class="col-md-2 mb-md-0 mb-3">
+
+        <h5 class="text-uppercase">Liens utiles</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://discord.gg/j5jz6MsjjZ" class="text-dark" target="_blank">Discord</a>
+          </li>
+          <li>
+            <a href="http://wiki.histeria.fr/" class="text-dark" target="_blank">Wiki</a>
+          </li>
+          <li>
+            <a href="http://shop.histeria.fr/" class="text-dark" target="_blank">Boutique</a>
+          </li>
+          <li>
+            <a href="http://vote.histeria.fr/" class="text-dark" target="_blank">Vote</a>
+          </li>
+        </ul>
+
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>Un espace vert  la joliverie</title>
+        <meta charset="utf-8">
+        <style type="text/css"> @import url(styles.css);</style>
+    </head>
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<header>
+<li><a href="index.php">home</li>
+    <li><a href="page1.php">lien page1</li>
+    <li><a href="page2.php">lien page2</li>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Un espace vert a la jol</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="page1.php">Lien page 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="page2.php">Lien page 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="page3.php">Lien page 3</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Un projet réalisé par un groupe du module tremplin</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+<body>
